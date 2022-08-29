283af5ccab07127885d4e902e9e83756c44b6ce9
Athipisut <athipisutj63@nu.ac.th>
2022-08-29 16:44:45 +0700

    codelab6

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzYzODk1LCJleHAiOjE2NjE3Njc0OTUsIm5iZiI6MTY2MTc2Mzg5NSwianRpIjoiSUZkTmJKNE1RMGFXOGpRWiIsInN1YiI6MTAsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.ue4-sdg_sKkrfPQn0hBUP5uid4F0s0bURVV7yA0_EJs"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzY1OTE4LCJleHAiOjE2NjE3Njk1MTgsIm5iZiI6MTY2MTc2NTkxOCwianRpIjoiT3BZVFNKblVQdXRIdFlkWiIsInN1YiI6MTAsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.-ntvxoz6z57KL6YS39Y2i_KYtsEzU70zIHI-iQxIFAE"; // put access token
 const student = {
   id_student:63367292,
   name: 'Athipisut', // replace with your full name.
@@ -18,7 +18,7 @@ exports.findStudentbyId = function (student_id, cb) {
             method: "GET",
             uri: EXTERNAL_API+student_id,
             headers: {
-                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzYzODk1LCJleHAiOjE2NjE3Njc0OTUsIm5iZiI6MTY2MTc2Mzg5NSwianRpIjoiSUZkTmJKNE1RMGFXOGpRWiIsInN1YiI6MTAsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.ue4-sdg_sKkrfPQn0hBUP5uid4F0s0bURVV7yA0_EJs`
+                Authorization: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzY1OTE4LCJleHAiOjE2NjE3Njk1MTgsIm5iZiI6MTY2MTc2NTkxOCwianRpIjoiT3BZVFNKblVQdXRIdFlkWiIsInN1YiI6MTAsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.-ntvxoz6z57KL6YS39Y2i_KYtsEzU70zIHI-iQxIFAE`,
             }
         }).then((response) => {
             console.log('Sent');
